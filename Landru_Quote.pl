ab1de07203c26803480145f9cca16173df32d7ea
David <zapwai@gmail.com>
2015-04-12 15:49:32 -0400

    first commit

---
+++ Landru_Quote.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+
+my $ChoresFolder="~/Dropbox/code/perl/chores/";
+
+print "Hark! THIS IS THE WORD OF LANDRU...\nYou are compelled to obey.\n\n";
+$a_result = `perl $ChoresFolder/chores.pl`;
+print $a_result;
+print "If these chores are not completed within one week...\n\t the offender will be absorbed.\n";
+print "\t\t\t For the good of the body and all that. \n\t\t\t\t ta ta for now, Landru\n";
+print "\n -- \n";
+print "Landru seeks tranquility. Peace for all. The universal good.";
+
+## Use LandruAbides21 to access Landru@ferroned.com
